69ce45d7d44e81d0f08e817884bd14ae73e49e57
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-12 09:43:49 -0600

    Finished

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -1,7 +1,7 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Chris Doyle
+  	Date: 11/12/2017
 */
 
 
@@ -14,11 +14,9 @@ public class P1_OutputLoopResult {
 	
 	public static void printSumOfSquaresTo5() {
 		for( int i = 1; i <= 5; i++ ) {
-			int finalResult = 0;
-			finalResult = finalResult + i * i;
-		}
+			int finalResult = i * i;
 		
-		System.out.println( "Final result is: " + finalResult );
+		System.out.println( "Final result is: " + finalResult);
 	}
 
-}
+}}
